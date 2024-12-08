afd2216dc99add538116fb34b0379a528b5f9bfb
umerfaro <muhammadranaumerofficial@gmail.com>
2024-12-08 20:43:39 +0500

    backend deploy to railway

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,3 +1,18 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       "/api/": "http://localhost:5000",
+//       "/uploads/": "http://localhost:5000",
+//     },
+//   },
+// });
+
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
@@ -6,8 +21,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api/": "http://localhost:5000",
-      "/uploads/": "http://localhost:5000",
+      "/api/": "https://webbackend-production-43d2.up.railway.app",
+      "/uploads/": "https://webbackend-production-43d2.up.railway.app",
     },
   },
 });
